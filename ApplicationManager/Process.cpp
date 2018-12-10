537a643defe5adebd3d0accc8154529b07de6505
jinneec <lvjm@163.com>
2018-12-10 21:11:12 +0800

    Avoid wait error <Interrupted system call>

--- ApplicationManager/Process.cpp
+++ ApplicationManager/Process.cpp
@@ -1,5 +1,6 @@
+#include <thread>
 #include "Process.h"
-
+#include "../common/Utility.h"
 
 
 Process::Process()
@@ -18,11 +19,25 @@ void Process::attach(int pid)
 
 void Process::killgroup()
 {
+	const static char fname[] = "Process::killgroup() ";
+
 	if (this->running() && this->getpid() > 1)
 	{
 		ACE_OS::kill(-(this->getpid()), 9);
 		this->terminate();
-		//avoid  zombie process
-		this->wait();
+		if (this->wait() < 0)
+		{
+			//avoid  zombie process (Interrupted system call)
+			LOG_WAR << fname << "Wait process <" << getpid() << "> to exit failed with error : " << std::strerror(errno);
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			if (this->wait() < 0)
+			{
+				LOG_ERR << fname << "Retry wait process <" << getpid() << "> failed with error : " << std::strerror(errno);
+			}
+			else
+			{
+				LOG_INF << fname << "Retry wait process <" << getpid() << "> success";
+			}
+		}
 	}
 }
